a439e35f81bdeb42ccc99fd07a1d464f1f96521a
Dan Stoner <danstoner@gmail.com>
2014-08-14 17:09:07 -0400

    add comments about fields wanted from API

--- dailymile_dump_json.py
+++ dailymile_dump_json.py
@@ -9,34 +9,61 @@ import calendar
 # dailymile user name
 dm_user="danstoner"
 
-## Pretty sure that the "since" parameter does not actually work in the API. I could not get 
-## unix timestamp to affect the results of the API calls in any way.
+
 # Earliest date entry to fetch in format YYYY-MM-DD
 start_date = "2012-01-01"
 
-
-# CODE
-
 # API needs unix time aka ticks since the epoch
 # See:
+#  http://www.dailymile.com/api/documentation
 #  https://www.dailymile.com/forums/bugs-and-support/topics/11340-api-question-about-getting-events-since-certain-date
 #  http://stackoverflow.com/questions/9637838/convert-string-date-to-timestamp-in-python
 # Example: January 1, 2010 or 2010-01-01 would become "1262304000"
 date_since = str(calendar.timegm(time.strptime(start_date,"%Y-%m-%d")))
 
-
-# Will just need to page through every single entry starting with page 1 until there
-# are no more pages.
-
-#api_url_entries="https://api.dailymile.com/people/" + dm_user + "/entries.json?since=" + date_since
-
+## Pretty sure that the "since" parameter does not actually work in the API. I could not get 
+## unix timestamp to affect the results of the API calls in any way.  So instead, 
+## will just page through every single entry starting with page 1 until there
+## are no more pages. This is actually reasonable for a "full export" anyway (until we blow out 
+#  some max requests limit... dailymile currently caps at 1500 requests per hour).
 
 # start at page 1 and go until we stop getting HTTP ok
-
 page = 1
+
+
+###### API fields
+# These are what we want out of the api
+#
+# id
+# url
+# at
+# message
+# workout
+#   activity_type
+#   distance
+#     value
+#     units
+#   felt
+#   duration
+#   title
+#
+# workout: {
+# activity_type: "Running",
+# distance: {
+# value: 3.8,
+# units: "miles"
+# },
+# felt: "good",
+# duration: 1620,
+# title: "Track workout"
+# }
+
+# CODE
 
 s = requests.Session()
 
+# sample url for page 1 would be:
+# https://api.dailymile.com/people/danstoner/entries.json?page=1
 api_url_entries="https://api.dailymile.com/people/" + dm_user + "/entries.json?page=" + str(page)
 
 #r = requests.get(api_url_entries)
