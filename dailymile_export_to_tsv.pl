4cb017cfde71bade6f1bb12c874c98c9fbad0f87
Dan Stoner <danstoner@gmail.com>
2015-05-04 21:33:53 -0400

    able to get some values out of the returned JSON

--- dailymile_export_to_tsv.pl
+++ dailymile_export_to_tsv.pl
@@ -83,25 +83,40 @@ open my $fh, ">:encoding(utf8)", $outputfilename or die "Could not write to ".$o
 
 my $status = $csv->print ($fh, $headerrow) or die "Could not write to ".$outputfilename." ".$!;
 
+# do the real work
+
 my $page = 1;
-
 my $api_url_entries = "https://api.dailymile.com/people/" . $dm_user . "/entries.json?page=" . $page;
-
 msg ( "First API Request: ". $api_url_entries , 1 );
-
 
 my $ua = LWP::UserAgent->new;
 my $response = $ua->get($api_url_entries);
 
-my $json = decode_json $response->decoded_content;
-print Dumper($json);
+my $json = decode_json $response->decoded_content; # $json becomes a hash
+
+# print Dumper($json);
+
+for my $entry ( @{$json->{entries}} )
+{
+    print $entry->{id} . "," . $entry->{url} . "\n";
+}
+
+
+#while (($response->is_success) and (exists $jsonscalar->{entries})) {
+#    print $response->decoded_content;  # or whatever
+
+#}
+
+die "die... debugging";
+#}
+#else {
+#    die $response->status_line;
+#}
+
+
+
 
 __END__
-
-## try to create output file here
-##
-##
-
 
 
 my $api_url_entries = "http://api.dailymile.com/people/" ~ $dm_user ~ "/entries.json?page=" ~ $page;
@@ -114,39 +129,67 @@ my $response = $ua.get($api_url_entries);
 if ! $response.is-success {
   die $response.status-line;
 } else {
-
-say from-json('{ "a": 42 }').perl;
-say to-json { a => [1, 2, 'b'] };
-
-# save this line for now
-my %rjson = %(from-json($response.content));
-
-
-#  my @rjson = %(from-json($response.content)){'entries'};
-
-### Whether I use an array or a hash, from-json seems to be stuffing everything
-### into a single element.
-#  say "................................................................";
-
-#   for %rjson{@x} {            
-#     $_.say;
-# }
-
-  
-
-#  my @entries = %(from-json($response.content)){'entries'}[0];
-#  my @entries = %(from-json($response.content)){'entries'}[1];
- 
-#  for @entries {
-# #    say $_ => 'id';
-#     say $_{'id'};
-#     # say "..................";
-#     }
-
+# do someting
 }
-
 
 
 say "**** Terminating on purpose.***";
 exit;
 # code below here not used or not ready
+
+
+##### sample of json (output by Dumper($json)
+# $VAR1 = {
+#           'entries' => [
+#                          {
+#                            'id' => 32646089,
+#                            'user' => {
+#                                        'username' => 'danstoner',
+#                                        'display_name' => 'Dan S.',
+#                                        'photo_url' => 'https://dnetd3r67cewl.cloudfront.net/unsafe/48x48/https://d2d6zexjsynj7u.cloudfront.net/pictures/users/93956/1429899791.jpg',
+#                                        'url' => 'http://www.dailymile.com/people/danstoner'
+#                                      },
+#                            'url' => 'http://www.dailymile.com/entries/32646089',
+#                            'geo' => {
+#                                       'coordinates' => [
+#                                                          '-82.36792',
+#                                                          '29.676735'
+#                                                        ],
+#                                       'type' => 'Point'
+#                                     },
+#                            'workout' => {
+#                                           'title' => 'Brywood to NW 31st Blvd ( Glen Springs Rd ) out and back',
+#                                           'duration' => 2520,
+#                                           'activity_type' => 'Running',
+#                                           'distance' => {
+#                                                           'units' => 'miles',
+#                                                           'value' => '4.04'
+#                                                         },
+#                                           'felt' => 'alright'
+#                                         },
+#                            'likes' => [],
+#                            'comments' => [
+#                                            {
+#                                              'created_at' => '2015-05-04T14:27:45Z',
+#                                              'body' => 'Nice Monday miles!',
+#                                              'user' => {
+#                                                          'url' => 'http://www.dailymile.com/people/BarbaraW',
+#                                                          'photo_url' => 'https://dnetd3r67cewl.cloudfront.net/unsafe/48x48/https://d2d6zexjsynj7u.cloudfront.net/pictures/users/99552/1409113516.jpg',
+#                                                          'username' => 'BarbaraW',
+#                                                          'display_name' => 'Barbara W.'
+#                                                        }
+#                                            }
+#                                          ],
+#                            'location' => {
+#                                            'name' => 'Gainesville, FL'
+#                                          },
+#                            'at' => '2015-05-04T10:25:29Z'
+#                          },
+#                          {
+#                            'url' => 'http://www.dailymile.com/entries/32637156',
+#                            'user' => {
+#                                        'display_name' => 'Dan S.',
+#                                        'username' => 'danstoner',
+#                                        'photo_url' => 'https://dnetd3r67cewl.cloudfront.net/unsafe/48x48/https://d2d6zexjsynj7u.cloudfront.net/pictures/users/93956/1429899791.jpg',
+#                                        'url' => 'http://www.dailymile.com/people/danstoner'
+#                                      }
