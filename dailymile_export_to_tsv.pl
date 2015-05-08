d5296109f39167388ca3adb7bcc34add98001439
Dan Stoner <danstoner@gmail.com>
2015-05-07 21:29:46 -0400

    perl 5 version working now, feature parity with python version (although slightly different command-line options). Perl output format is comma-delimited rather than tab-delimited.

--- dailymile_export_to_tsv.pl
+++ dailymile_export_to_tsv.pl
@@ -1,11 +1,10 @@
 use LWP::UserAgent;
 use JSON::PP;
-#use JSON qw (decode_json);
 use Text::CSV;
 use Getopt::Long;
 use POSIX qw(strftime);
 use Log::Message::Simple qw[msg error debug];
-use Data::Dumper; # mostly during debugging and testing
+use Data::Dumper; # for debugging and testing
 
 use strict;
 use warnings;
@@ -28,12 +27,12 @@ Usage: dailymile_export_to_tsv.pl [OPTIONS] <PARAMETERS>
   Parameters:
     --help, -h         Display this usage help.
     --username, -u USERNAME
-                       The dailymile.com username to export.
+                       The dailymile.com username to export (Required).
   Options:
     --debug, -d        Enable debug level output.
     --gear, -g         Enable download of gear info (not yet available)
     --maxpages, -m MAX
-                       Maximum number of pages to fetch (to limit http requests)
+                       Maximum number of pages to fetch (to limit http requests during testing)
 
 END
 
@@ -64,7 +63,6 @@ if (!($dm_user)) {
     usage();
 }
 
-
 if ($debug) {
     debug ("DEBUG enabled.",1);
 }
@@ -73,17 +71,13 @@ if (! $maxpages) {
     $maxpages = 1000;
 }
 
-print "Max $maxpages \n";
-# Begin
-
 my $now_string = strftime "%Y%m%d%H%M%S", localtime;
 
 my $outputfilename = $dm_user."_dailymile_export_p5.".$now_string.".csv";
 
 my $headerrow = ["id","url","timestamp","title","activity_type","felt","duration_seconds","distance","distance_units","description"];
-#["id","url","timestamp","title","activity_type","felt","duration_seconds","distance","distance_units","description"]
 
-my $csv = Text::CSV->new ( { binary => 1 } );
+my $csv = Text::CSV->new ( { binary => 1 ,eol => "\n"} );
 $csv->column_names ($headerrow);
 
 # If cannot write to file, might as well stop here
@@ -96,7 +90,7 @@ my $status = $csv->print ($fh, $headerrow) or die "Could not write to ".$outputf
 my $page = 1;
 my $keep_going = 1;
 my $api_url_entries = "https://api.dailymile.com/people/" . $dm_user . "/entries.json?page=" . $page;
-msg ( "First API Request: ". $api_url_entries , 1 );
+msg ( "Fetching: ". $api_url_entries , 1 );
 
 my $ua = LWP::UserAgent->new;
 my $response = $ua->get($api_url_entries);
@@ -111,41 +105,14 @@ if (!(exists ($json->{entries}[0]))) {
 
 my %results;
 
-# ## Process the page
-#     if( $pages_left && defined $data )
-# {
-#   ENTRIES: foreach my $entry ( @{ $data->{'entries'} } )
-#   {
-#         # print Dumper( $entry );    
-
-#         # See if we've gone past the start of the year  
-#       if( $entry->{'at'} =~ /2010/ )
-#       {
-#           $pages_left = 0;
-#           last ENTRIES;
-#       }
-      
-# #      if( $entry->{'workout'}{'activity_type'} eq 'Running' )
-#       { 
-# #          my ( $date ) = ( $entry->{'at'} =~ /([0-9-]+)T/ ); 
-#           # msg( $date . "," . $entry->{'workout'}{'distance'}{'value'} );
-      
-#           my %run =
-#           ( 
-#             'date'     => $date,
-# #            'distance' =>  $entry->{'workout'}{'distance'}{'value'}
-#           );
-      
-
 while (($response->is_success) && $keep_going && ($page < $maxpages)) {
        for my $entry ( @{$json->{entries}} )
        {
+	   # id, url, always seem to be present in source data. 
 	   my $id = $entry->{id};
 	   my $url = $entry->{url};
 	   my $timestamp = $entry->{at};
-	   #debug("id: " . $id ,1);
-	   # id, url, always seem to be present in source data.
-
+	   # Missing data in JSON will show up as undef in the %results.
 	   my $title = $entry->{workout}{title};
 	   my $activity_type = $entry->{workout}{activity_type};
 	   my $felt = $entry->{workout}{felt};
@@ -153,15 +120,6 @@ while (($response->is_success) && $keep_going && ($page < $maxpages)) {
 	   my $distance = $entry->{workout}{distance}{value};
 	   my $distance_units = $entry->{workout}{distance}{units};
 	   my $description = $entry->{message};
-### Save this block in case we need to replace any empty values (undef) with empty string.
-	   # if ( ! defined $title ) {$title="";}
-	   # if ( ! defined $activity_type ) {$activity_type = "";}
-	   # if ( ! defined $felt ) {$felt = "";}
-	   # if ( ! defined $duration_seconds ) {$duration_seconds = "";}
-	   # if ( ! defined $distance ) {$distance = "";}
-	   # if ( ! defined $distance_units ) {$distance_units = "";}
-	   # if ( ! defined $description ) {$description = "";}
-###
 	   @{$results{$id}} = (
 	       $id, 
 	       $url, 
@@ -195,35 +153,12 @@ while (($response->is_success) && $keep_going && ($page < $maxpages)) {
 # ready for rows output
 
 for my $key (sort(keys %results)) {
-#    msg ("key: ". $key, 1);
-    my $colref = (@{%results->{$key}});
-    #print @colref;
-    $csv->print ($fh, $colref);  
+    my $colref = \@{$results{$key}};
+    $csv->print ($fh, $colref);
 }
 
-
-die "die... __END__";
 
 __END__
-
-my $api_url_entries = "http://api.dailymile.com/people/" ~ $dm_user ~ "/entries.json?page=" ~ $page;
-
-say "First API Request: " ~ $api_url_entries;
-
-my $ua = HTTP::UserAgent.new;
-my $response = $ua.get($api_url_entries);
-
-if ! $response.is-success {
-  die $response.status-line;
-} else {
-# do someting
-}
-
-
-
-say "**** Terminating on purpose.***";
-exit;
-# code below here not used or not ready
 
 
 ##### sample of json (output by Dumper($json)
