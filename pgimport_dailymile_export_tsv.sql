1abc470f25fe061a8a1aa5598cb8aee9d4052cd2
Dan Stoner <danstoner@gmail.com>
2016-06-17 15:07:55 -0400

    update sql for added columns "effort_outof_5","gear","weather","calories"

--- pgimport_dailymile_export_tsv.sql
+++ pgimport_dailymile_export_tsv.sql
@@ -13,10 +13,15 @@ DROP TABLE imported_entries;
 # postgres is awesome. We can be a little better about our column definitions than importing everything as text.
 # posgres "does the right thing" with the import (via the COPY command)
 
-CREATE table imported_entries (id integer primary key, url text, entry_date timestamp, title text, activity_type text, felt text, duration_seconds integer, distance NUMERIC(7,4), distance_units text, description text);
+# header = ["id","url","timestamp","title","activity_type","felt","duration_seconds","distance","distance_units","description","effort_outof_5","gear","weather","calories"]
+CREATE table imported_entries (id integer primary key, url text, entry_date timestamp, title text, activity_type text, felt text, duration_seconds integer, distance NUMERIC(7,4), distance_units text, description text, effort_outof_5 integer, gear text, weather text, calories integer);
 
 # Note that a common issue with importing into postgresql has to do with
 # file path / location and file permissions.
 # I place the .tsv export file in /tmp before trying to load it into the db.
 
 COPY imported_entries FROM '/tmp/danstoner_dailymile_export.tsv' WITH DELIMITER AS '\t' CSV HEADER;
+
+## Alternative command using the postgresql \COPY command. Note there is no semicolon for \ commands.
+##
+## \COPY imported_entries FROM '/tmp/danstoner_dailymile_export.tsv' WITH DELIMITER AS '\t' CSV HEADER
