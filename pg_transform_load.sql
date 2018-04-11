c15db60c21d9c806f6577bcedb644e960782cb74
Dan Stoner <danstoner@gmail.com>
2018-04-11 19:43:27 -0400

    fix column names and distance UPDATE queries to actually work

--- pg_transform_load.sql
+++ pg_transform_load.sql
@@ -57,14 +57,17 @@ CREATE TABLE  entries (
 -- Transform and load the entries table from the raw import table 
 --
 \echo Inserting...
-INSERT INTO entries (id, url, entry_date, title, activity_type, felt, duration_time_seconds, description, effort_outof_5, gear, weather, calories)
-SELECT id, url, entry_date, title, activity_type, felt, duration_time_seconds, description, effort_outof_5, gear, weather, calories from imported_entries;
+INSERT INTO entries (id, url, entry_date, title, activity_type, felt, description, effort_outof_5, gear, weather, calories)
+SELECT id, url, entry_date, title, activity_type, felt, description, effort_outof_5, gear, weather, calories from imported_entries;
 
 \echo Updating distance_in_km
-UPDATE entries SET distance_in_km = miles_to_km (i.miles) FROM imported_entries i where i.units = 'miles' AND id = i.id;
+UPDATE entries SET distance_in_km = miles_to_km (i.distance) FROM imported_entries i where i.distance_units = 'miles' AND entries.id = i.id;
 
 \echo Updating distance_in_miles
-UPDATE entries SET distance_in_miles = km_to_miles (i.km) FROM imported_entries i where i.units = 'kilometers' AND id = i.id;
+UPDATE entries SET distance_in_miles = km_to_miles (i.distance) FROM imported_entries i where i.distance_units = 'kilometers' AND entries.id = i.id;
+
+\echo updating duration_time_seconds
+-- UPDATE entries SET duration_time_seconds = ...
 
 \echo Setting pace_per_mile
 -- UPDATE entries SET pace_per_mile = ...
