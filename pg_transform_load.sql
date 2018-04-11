caf2125c41e6920d79aa7ce73175a92a5a4c75df
Dan Stoner <danstoner@gmail.com>
2018-04-11 18:18:28 -0400

    re-order to create functions first, stub update commands

--- pg_transform_load.sql
+++ pg_transform_load.sql
@@ -1,19 +1,9 @@
 -- for PostgreSQL
 --
 
--- There is no "IF EXISTS" in older versions of postgres
-DROP TABLE entries;
-
-CREATE TABLE  entries (
-       id INTEGER PRIMARY KEY, 
-       url TEXT, entry_date TIMESTAMP, 
-       title TEXT, activity_type TEXT, felt TEXT, duration_time_seconds INTERVAL, 
-       distance_in_miles NUMERIC(7,4), distance_in_km NUMERIC(7,4), 
-       description TEXT, effort_outof_5 INTEGER, gear TEXT, weather TEXT, calories INTEGER, 
-       pace_per_mile INTERVAL, pace_per_km INTERVAL
-);
-
+--
 -- Create some useful stored procedures for runners
+--
 
 CREATE OR REPLACE FUNCTION miles_to_km (miles NUMERIC) 
 RETURNS NUMERIC AS $$
@@ -43,3 +33,34 @@ $$ LANGUAGE plpgsql;
 -- (1 row)
 
 
+
+--
+-- Create the entries table
+--
+
+-- There is no "IF EXISTS" in older versions of postgres
+DROP TABLE entries;
+
+CREATE TABLE  entries (
+       id INTEGER PRIMARY KEY, 
+       url TEXT, entry_date TIMESTAMP, 
+       title TEXT, activity_type TEXT, felt TEXT, duration_time_seconds INTERVAL, 
+       distance_in_miles NUMERIC(7,4), distance_in_km NUMERIC(7,4), 
+       description TEXT, effort_outof_5 INTEGER, gear TEXT, weather TEXT, calories INTEGER, 
+       pace_per_mile INTERVAL, pace_per_km INTERVAL
+);
+
+
+--
+-- Transform and load the entries table from the raw import table 
+--
+
+-- INSERT INTO ...
+
+-- UPDATE entries SET distance_in_km = miles_to_km (miles) where t1.units = 'miles' ...
+
+-- UPDATE entries SET distance_in_miles = km_to_miles (km) where t1.unites = 'kilometers' ...
+
+-- UPDATE entries SET pace_per_mile = ...
+
+-- UPDATE entries SET pace_per_km = ...
