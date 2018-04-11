afd64c6e9b5050d4538c2d40d0f0f81b20d84248
Dan Stoner <danstoner@gmail.com>
2018-04-10 20:10:42 -0400

    add stored procedures with useful running calculations
    Running log will contain distances such as 5k or 10k.
    When we convert to miles, we will want these to be the "common"
    conversion distance rather than the actual precise distances.
    This should make sorting, ranges, and grouping more useful.

--- pg_transform_load.sql
+++ pg_transform_load.sql
@@ -13,3 +13,34 @@ CREATE TABLE  entries (
        pace_per_mile INTERVAL, pace_per_km INTERVAL
 );
 
+-- Create some useful stored procedures for runners
+
+
+CREATE OR REPLACE FUNCTION miles_to_km (miles NUMERIC(3,2))
+RETURNS NUMERIC(3,2) AS $$
+BEGIN
+RETURN (miles * 1.61)::NUMERIC(3,1);
+END;
+$$ LANGUAGE plpgsql;
+-- SAMPLE miles_to_km
+-- fitness=# select miles_to_km(3.1);
+--  miles_to_km 
+-- -------------
+--          5.0
+-- (1 row)
+
+
+CREATE OR REPLACE FUNCTION km_to_miles (km NUMERIC(3,2))
+RETURNS NUMERIC(3,2) AS $$
+BEGIN
+RETURN (km * 0.62)::NUMERIC(3,1);
+END;
+$$ LANGUAGE plpgsql;
+-- SAMPLE km_to_miles
+-- fitness=# select km_to_miles(10);
+--  km_to_miles 
+-- -------------
+--          6.2
+-- (1 row)
+
+
