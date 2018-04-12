08a1071a71a37a667f40cb714638523e5a0e8cb1
Dan Stoner <danstoner@gmail.com>
2018-04-12 18:59:22 -0400

    release etl that builds running_entries table

--- pg_transform_load.sql
+++ pg_transform_load.sql
@@ -35,18 +35,18 @@ $$ LANGUAGE plpgsql;
 
 
 --
--- Create the entries table
+-- Create the running entries table
 --
 
-\echo Drop and Create table "entries"
+\echo Drop and Create table "running_entries"
 
 -- There is no "IF EXISTS" in older versions of postgres
-DROP TABLE entries;
+DROP TABLE running_entries;
 
-CREATE TABLE  entries (
+CREATE TABLE  running_entries (
        id INTEGER PRIMARY KEY, 
        url TEXT, entry_date TIMESTAMP, 
-       title TEXT, activity_type TEXT, felt TEXT, elapsed_time INTERVAL NOT NULL DEFAULT '59:59:59'::interval, 
+       title TEXT, felt TEXT, elapsed_time INTERVAL NOT NULL DEFAULT '59:59:59'::interval, 
        distance_in_miles NUMERIC(7,4) DEFAULT 0, distance_in_km NUMERIC(7,4) DEFAULT 0, 
        description TEXT, effort_outof_5 INTEGER, gear TEXT, weather TEXT, calories INTEGER, 
        pace_per_mile INTERVAL, pace_per_km INTERVAL
@@ -56,41 +56,42 @@ CREATE TABLE  entries (
 --
 -- Transform and load the entries table from the raw import table 
 --
-\echo Inserting...
-INSERT INTO entries (id, url, entry_date, title, activity_type, felt, description, effort_outof_5, gear, weather, calories)
-SELECT id, url, entry_date, title, activity_type, felt, description, effort_outof_5, gear, weather, calories from imported_entries;
+\echo Inserting running entries...
+INSERT INTO running_entries (id, url, entry_date, title, felt, description, effort_outof_5, gear, weather, calories)
+SELECT id, url, entry_date, title, felt, description, effort_outof_5, gear, weather, calories from imported_entries
+WHERE activity_type = 'Running';
 
 \echo Updating distance_in_km
-UPDATE entries SET distance_in_km = miles_to_km (i.distance) FROM imported_entries i where i.distance_units = 'miles' AND entries.id = i.id;
-UPDATE entries SET distance_in_km = i.distance FROM imported_entries i where i.distance_units = 'kilometers' AND entries.id = i.id;
+UPDATE running_entries SET distance_in_km = miles_to_km (i.distance) FROM imported_entries i where i.distance_units = 'miles' AND running_entries.id = i.id;
+UPDATE running_entries SET distance_in_km = i.distance FROM imported_entries i where i.distance_units = 'kilometers' AND running_entries.id = i.id;
 
 \echo Updating distance_in_miles
-UPDATE entries SET distance_in_miles = km_to_miles (i.distance) FROM imported_entries i where i.distance_units = 'kilometers' AND entries.id = i.id;
-UPDATE entries SET distance_in_miles = i.distance FROM imported_entries i where i.distance_units = 'miles' AND entries.id = i.id;
+UPDATE running_entries SET distance_in_miles = km_to_miles (i.distance) FROM imported_entries i where i.distance_units = 'kilometers' AND running_entries.id = i.id;
+UPDATE running_entries SET distance_in_miles = i.distance FROM imported_entries i where i.distance_units = 'miles' AND running_entries.id = i.id;
 
 
 \echo updating duration_time_seconds
-UPDATE entries SET elapsed_time = COALESCE(i.duration_seconds * INTERVAL '1 second', '0 seconds'::INTERVAL) FROM imported_entries i WHERE entries.id = i.id;
+UPDATE running_entries SET elapsed_time = COALESCE(i.duration_seconds * INTERVAL '1 second', '0 seconds'::INTERVAL) FROM imported_entries i WHERE running_entries.id = i.id;
 
 \echo Setting pace_per_mile
-UPDATE entries SET pace_per_mile = elapsed_time / distance_in_miles where distance_in_miles != 0;
+UPDATE running_entries SET pace_per_mile = elapsed_time / distance_in_miles where distance_in_miles != 0;
+UPDATE running_entries SET pace_per_mile = '59:59:59' where pace_per_mile = '00:00:00';
 
 \echo Setting pace_per_km
-UPDATE entries SET pace_per_km = elapsed_time / distance_in_km where distance_in_km != 0;
+UPDATE running_entries SET pace_per_km = elapsed_time / distance_in_km where distance_in_km != 0;
+UPDATE running_entries SET pace_per_km = '59:59:59' where pace_per_km = '00:00:00';
 
--- TO DO  more thinking about NULL situations... where elapsed time is 0, distance is 0, pace is null or 0 or largest number for sorting?
--- NULL sorts at opposite end of ASC/DESC than 00:00:00 (zero elapsed time).
 
 -- Sample:
 -- fitness=# select id,title, elapsed_time, distance_in_miles, distance_in_km, pace_per_mile, pace_per_km 
--- from entries WHERE pace_per_mile between '5 minutes'::interval AND '7 minutes'::interval order by pace_per_mile limit 20; 
+-- from running_entries WHERE pace_per_mile between '5 minutes'::interval AND '7 minutes'::interval order by pace_per_mile limit 20; 
 --     id    |                 title                  | elapsed_time | distance_in_miles | distance_in_km |  pace_per_mile  |   pace_per_km   
 -- ----------+----------------------------------------+--------------+-------------------+----------------+-----------------+-----------------
 --  35621592 | Track meet - 1500m race                | 00:04:48     |            0.9300 |         1.5000 | 00:05:09.677419 | 00:03:12
 --  30113586 | Track Birthday Mile                    | 00:05:13     |            1.0000 |         1.6000 | 00:05:13        | 00:03:15.625
 --  35646080 | Track - 1 Mile time trial              | 00:05:17     |            1.0000 |         1.6000 | 00:05:17        | 00:03:18.125
 --  29211800 | Track meet 1500m                       | 00:04:46     |            0.9000 |         1.5000 | 00:05:17.777778 | 00:03:10.666667
---  24524049 | Time trial 1600m and 400m              | 00:06:45     |            1.2500 |         2.0000 | 00:05:24        | 00:03:22.5
+0--  24524049 | Time trial 1600m and 400m              | 00:06:45     |            1.2500 |         2.0000 | 00:05:24        | 00:03:22.5
 --  34560219 | Florida Track Club - Shorter Mile race | 00:05:25     |            1.0000 |         1.6000 | 00:05:25        | 00:03:23.125
 --  32994501 | Track meet 1500m                       | 00:04:53     |            0.9000 |         1.5000 | 00:05:25.555556 | 00:03:15.333333
 --  36685940 | The Frank Shorter Mile race            | 00:05:27     |            1.0000 |         1.6000 | 00:05:27        | 00:03:24.375
